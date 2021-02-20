5d7e652ad8b8b6b0a643de1ed7e527889e5bd8b7
unknown <jjj1305@hanmail.net>
2021-02-21 00:53:52 +0900

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -101,7 +101,7 @@ else:
         'default': {
             'ENGINE': 'django.db.backends.postgresql',
             'HOST': os.environ.get("RDS_HOST"),
-            'NAME': os.environ.get("RDS_NAME"),
+            # 'NAME': os.environ.get("RDS_NAME"),
             'USER': os.environ.get("RDS_USER"),
             "PASSWORD": os.environ.get("RDS_PASSWORD"),
             "PORT": "5432",
